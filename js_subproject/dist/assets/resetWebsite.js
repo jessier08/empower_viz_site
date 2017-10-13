9ee655ece63fb3b4c9e4959ab7eca0246a9b9ab8
Navarjun <navarjun@Navarjuns-MacBook-Pro.local>
2017-10-13 13:27:43 -0400

    -Page redirect back to landing

---
+++ js_subproject/dist/assets/resetWebsite.js
@@ -0,0 +1,21 @@
+var inactivityTime = function () {
+    var t;
+    window.onload = resetTimer;
+    // DOM Events
+    document.onload = resetTimer;
+    document.onmousemove = resetTimer
+    document.onmousedown = resetTimer // touchscreen presses
+    document.ontouchstart = resetTimer
+    document.onclick = resetTimer     // touchpad clicks
+    document.onscroll = resetTimer    // scrolling with arrow keys
+    document.onkeypress = resetTimer
+
+    function resetTimer () {
+        clearTimeout(t);
+        t = setTimeout(function () {
+            window.location.replace('/')
+        }, 180000)
+        // 1000 milisec = 1 sec, so this is 3 minutes
+    }
+};
+inactivityTime()
